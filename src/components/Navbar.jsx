aef4ebb3caeaf4c84b3120e79590fda2b31ca0d5
Pierre <pierre.nogaro@gmail.com>
2025-03-08 15:20:24 +0100

    bootstrap, navbar & router-dom

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,23 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <div className="container">
+                <Link className="navbar-brand" to="/">Alcool App</Link>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    <ul className="navbar-nav ms-auto">
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/">Home</Link>
+                        </li>
+                        <li className="nav-item">
+                            <Link className="nav-link" to="/alcool">Alcool</Link>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/pages/Alcool.jsx
@@ -0,0 +1,9 @@
+const Alcool = () => {
+    return (
+        <div>
+            <h1>Alcool Page</h1>
+        </div>
+    )
+}
+
+export default Alcool
